8a94f98f6e5bc4dcc3f4ec9fcf7d9f5e0ddf5b5a
Bonnie-TJ <e1335039@u.nus.edu>
2024-10-19 13:56:29 +0800

    fix startup error

--- src/main/java/com/nus/edu/se/api_gateway/config/CorsConfigController.java
+++ src/main/java/com/nus/edu/se/api_gateway/config/CorsConfigController.java
@@ -1,20 +1,16 @@
 package com.nus.edu.se.api_gateway.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfigController implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("*") // Allow all origins
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow sending cookies
-    }
-
-}
+//@Configuration
+//public class CorsConfigController implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Apply CORS to all endpoints
+//                .allowedOrigins("*") // Allow all origins
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific methods
+//                .allowedHeaders("*") // Allow all headers
+//                .allowCredentials(true); // Allow sending cookies
+//    }
+//
+//}
